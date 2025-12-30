0748da478134c938765537f8aa1ef18cda095c2b
DORAEMONlovesMICHAN <mushamsushanth@gmail.com>
2025-12-30 13:41:19 +0530

    dotenv config fix

--- backend/routes/judge.js
+++ backend/routes/judge.js
@@ -3,9 +3,7 @@ const User = require("../models/User");
 const router = express.Router();
 const axios = require("axios");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 
